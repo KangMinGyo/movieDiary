016c8b7aa0b476650948fb95015027c7c38d3796
KangMingyo <kang1@KangMingyoui-MacBookPro.local>
2022-06-27 16:52:33 +0900

    First Commit

---
+++ movieDiary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  movieDiary
+//
+//  Created by KangMingyo on 2022/06/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

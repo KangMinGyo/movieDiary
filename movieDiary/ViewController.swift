d07ce96c31e6916adec8f5a15eea39d57af76cc8
KangMingyo <kang1@KangMingyoui-MacBookPro.local>
2022-06-29 00:17:06 +0900

    feat: Add TableView

--- movieDiary/ViewController.swift
+++ movieDiary/ViewController.swift
@@ -7,8 +7,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+    }
+    
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.

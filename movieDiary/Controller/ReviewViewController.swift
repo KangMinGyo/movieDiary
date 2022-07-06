12198b34eb45851c990d00d005ae6858371632a0
KangMingyo <kang15567@kakao.com>
2022-07-06 17:17:22 +0900

    feat: 별점시스템 추가

--- movieDiary/Controller/ReviewViewController.swift
+++ movieDiary/Controller/ReviewViewController.swift
@@ -12,6 +12,9 @@ class ReviewViewController: UIViewController {
     @IBOutlet weak var movieNameLabel: UILabel!
     @IBOutlet weak var reviewTextView: UITextView!
     
+    @IBOutlet weak var starSlider: UISlider!
+    @IBOutlet weak var starStackView: UIStackView!
+    
     var movieName : String = ""
     var movieInfo : String = ""
     
@@ -20,6 +23,30 @@ class ReviewViewController: UIViewController {
         self.movieNameLabel.text = movieName
     }
     
+    // MARK: - 별점 시스템
+    @IBAction func moveStarSlider(_ sender: UISlider) {
+        var value = starSlider.value
+        print(value)
+        
+        // star.leadinghalf.filled
+        //starImage.image = UIImage(systemName: "star.fill")
+        
+        for index in 0...5 {
+            if let starImage = view.viewWithTag(index) as? UIImageView {
+                if value > 0.5 {
+                    value -= 1
+                    starImage.image = UIImage(systemName: "star.fill")
+                } else if 0 < value && value < 0.5 {
+                    value -= 0.5
+                    starImage.image = UIImage(systemName: "star.leadinghalf.filled")
+                } else {
+                    starImage.image = UIImage(systemName: "star")
+                }
+        }
+    }
+}
+    
+    // MARK: - 메모 입력
 
     @IBAction func saveButton(_ sender: UIButton) {
         guard let review = reviewTextView.text, review.count > 0 else {
@@ -30,7 +57,7 @@ class ReviewViewController: UIViewController {
         self.presentingViewController?.dismiss(animated: true)
     }
 
-    //키보드 내리기
+    // MARK: - 키보드 내리기
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         view.endEditing(true)
     }
